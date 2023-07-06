51719a4e5e1bf796160d6e162908253329ed5e1f
manoj-shetty <manojs231@outlook.com>
2023-07-06 20:03:40 +0530

    encrypted password using bcryptjs in nodejs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'M@noj'
 
 // Create a User using: POST "/api/auth/createuser". Dosen't require Auth
 router.post('/createuser', [
@@ -23,14 +26,22 @@ router.post('/createuser', [
                 return res.status(400).json({ error: "Sorry a user with this email already exists" })
             }
 
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
             user = await User.create({
                 name: req.body.name,
-                password: req.body.password,
+                password: secPass,
                 email: req.body.email,
-            })
+            });
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authtoken = jwt.sign(data, JWT_SECRET);
 
-
-            res.json({ user })
+            // res.json({ user })
+            res.json({ authtoken })
         } catch (error) {
             console.error(error.message);
             res.status(500).send("Some Error occured");
