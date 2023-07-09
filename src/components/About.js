f1d80e0d5bc29a1413149b2893a0003d664e6e69
manoj-shetty <manojs231@outlook.com>
2023-07-09 19:30:48 +0530

    useContext is implemented

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import noteContext from '../context/notes/NoteContext'
 
 export const About = () => {
-  return (
-    <div>
-        This is About
-    </div>
-  )
+    const a = useContext(noteContext)
+    useEffect(() => {
+      a.update();
+      // eslint-disable-next-line
+    }, [])
+    
+    return (
+        <div>
+            This is About {a.state.name} and he is in class {a.state.class}
+        </div>
+    )
 }
 
 export default About
